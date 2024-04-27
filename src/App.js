76509db0ee85529cb216b837e0dfac7a3a11fc88
han ye ji <hyj99811@gmail.com>
2024-04-27 16:50:40 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,43 +1,72 @@
+import React from "react";
 import "./App.css";
 import Customer from "./components/Customer";
 import Image1 from "./img/profile(1).jpg";
 import Image2 from "./img/profile(1).jpg";
 import Image3 from "./img/profile(1).jpg";
+import { Paper, Table, TableHead, TableBody, TableCell, TableRow } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+// customers 배열 정의
+const customers = [
+  {
+    id: 1,
+    image: Image1,
+    name: "홍길동",
+    birthday: "980122",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: Image2,
+    name: "이순신",
+    birthday: "960727",
+    gender: "남자",
+    job: "장군",
+  },
+  {
+    id: 3,
+    image: Image3,
+    name: "아따맘마",
+    birthday: "890205",
+    gender: "여자",
+    job: "주부",
+  },
+];
+
+// RootPaper, RootTable 컴포넌트는 여전히 사용 가능합니다.
+const RootPaper = styled(Paper)(({ theme }) => ({
+  width: "100%",
+  marginTop: theme.spacing(3),
+  overflowX: "auto",
+}));
+
+const RootTable = styled(Table)(() => ({
+  minWidth: 1080,
+}));
 
 function App() {
-  const customers = [
-    {
-      id: 1,
-      image: Image1,
-      name: "홍길동",
-      birthday: "980122",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 2,
-      image: Image2,
-      name: "이순신",
-      birthday: "960727",
-      gender: "남자",
-      job: "장군",
-    },
-    {
-      id: 3,
-      image: Image3,
-      name: "아따맘마",
-      birthday: "890205",
-      gender: "여자",
-      job: "주부",
-    },
-  ];
-
   return (
-    <div>
-      {customers.map((customer) => (
-        <Customer key={customer.id} id={customer.id} image={customer.image} name={customer.name} birthday={customer.birthday} gender={customer.gender} job={customer.job} />
-      ))}
-    </div>
+    <RootPaper>
+      <RootTable>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((customer) => (
+            <Customer key={customer.id} id={customer.id} image={customer.image} name={customer.name} birthday={customer.birthday} gender={customer.gender} job={customer.job} />
+          ))}
+        </TableBody>
+      </RootTable>
+    </RootPaper>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,36 +1,52 @@
 import React from "react";
-
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
 function Customer({ name, birthday, gender, job, id, image }) {
-  return (
-    <div>
-      <CustomerProfile id={id} image={image} name={name} />
-      <CustomerInfo birthday={birthday} gender={gender} job={job} />
-    </div>
-  );
-}
-
-function CustomerProfile({ image, name, id }) {
   const imgStyle = {
     width: "100px",
     height: "100px",
     borderRadius: "50%",
   };
   return (
-    <div>
-      <img src={image} style={imgStyle} alt="profile"></img>
-      <h2>
-        {name}({id})
-      </h2>
-    </div>
+    <TableRow>
+      <TableCell>{id}</TableCell>
+      <TableCell>
+        <img src={image} alt="profileImg" style={imgStyle}></img>
+      </TableCell>
+      <TableCell>{name}</TableCell>
+      <TableCell>{birthday}</TableCell>
+      <TableCell>{gender}</TableCell>
+      <TableCell>{job}</TableCell>
+    </TableRow>
+    // <div>
+    //   <CustomerProfile id={id} image={image} name={name} />
+    //   <CustomerInfo birthday={birthday} gender={gender} job={job} />
+    // </div>
   );
 }
-function CustomerInfo({ birthday, gender, job }) {
-  return (
-    <div>
-      <p>{birthday}</p>
-      <p>{gender}</p>
-      <p>{job}</p>
-    </div>
-  );
-}
+
+// function CustomerProfile({ image, name, id }) {
+//   const imgStyle = {
+//     width: "100px",
+//     height: "100px",
+//     borderRadius: "50%",
+//   };
+//   return (
+//     <div>
+//       <img src={image} style={imgStyle} alt="profile"></img>
+//       <h2>
+//         {name}({id})
+//       </h2>
+//     </div>
+//   );
+// }
+// function CustomerInfo({ birthday, gender, job }) {
+//   return (
+//     <div>
+//       <p>{birthday}</p>
+//       <p>{gender}</p>
+//       <p>{job}</p>
+//     </div>
+//   );
+// }
 export default Customer;
